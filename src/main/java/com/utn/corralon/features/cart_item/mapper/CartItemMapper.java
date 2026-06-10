7e7c248a4367d11ffe5c8f9e7927a309972295d1
Mateo Maldini <mateomaldini.dev@gmail.com>
2026-06-10 11:00:40 -0300

    feat: update CartItemMapper to include product details in response and improve entity mapping

--- src/main/java/com/utn/corralon/features/cart_item/mapper/CartItemMapper.java
+++ src/main/java/com/utn/corralon/features/cart_item/mapper/CartItemMapper.java
@@ -1,30 +1,32 @@
 package com.utn.corralon.features.cart_item.mapper;
 
-import com.utn.corralon.features.brand.dto.BrandRequestDTO;
-import com.utn.corralon.features.brand.dto.BrandResponseDTO;
-import com.utn.corralon.features.brand.entity.BrandEntity;
+import com.utn.corralon.features.cart_item.dto.CartItemRequestDTO;
 import com.utn.corralon.features.cart_item.dto.CartItemResponseDTO;
 import com.utn.corralon.features.cart_item.entity.CartItemEntity;
-import jakarta.validation.constraints.NotNull;
 import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
 
+@Component
 public class CartItemMapper {
-    public ModelMapper modelMapper = new ModelMapper();
+    private final ModelMapper modelMapper;
 
     public CartItemMapper(ModelMapper modelMapper) {
         this.modelMapper = modelMapper;
     }
 
-    public CartItemResponseDTO toResponse(CartItemEntity cart) {
-        return modelMapper.map(cart, CartItemResponseDTO.class);
+    public CartItemResponseDTO toResponse(CartItemEntity entity) {
+        CartItemResponseDTO dto = modelMapper.map(entity, CartItemResponseDTO.class);
+        dto.setProductName(entity.getProductVariant().getProduct().getName());
+        dto.setVariantAttribute(entity.getProductVariant().getAttribute());
+        dto.setUnitPrice(entity.getProductVariant().getPrice());
+        return dto;
     }
 
-    public CartItemEntity toEntity(@NotNull CartItemResponseDTO dto) {
+    public CartItemEntity toEntity(CartItemRequestDTO dto) {
         return modelMapper.map(dto, CartItemEntity.class);
     }
 
-    public void updateEntity(CartItemEntity cart,
-                             CartItemResponseDTO dto) {
-        modelMapper.map(dto, cart);
+    public void updateEntity(CartItemEntity entity, CartItemRequestDTO dto) {
+        entity.setQuantity(dto.getQuantity());
     }
 }
