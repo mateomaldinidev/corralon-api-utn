49d3c5bc948d78e49f5195aa883cf70ccf870acc
Mateo Maldini <mateomaldini.dev@gmail.com>
2026-06-10 11:04:09 -0300

    refactor: change modelMapper fields to private final in BrandMapper, CategoryMapper, and SupplierMapper

--- src/main/java/com/utn/corralon/features/category/mapper/CategoryMapper.java
+++ src/main/java/com/utn/corralon/features/category/mapper/CategoryMapper.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class CategoryMapper {
 
-    public ModelMapper modelMapper;
+    private final ModelMapper modelMapper;
 
     public CategoryMapper(ModelMapper modelMapper) {
         this.modelMapper = modelMapper;
